63ef0571c7d2e8708e635adad3c6b01a43cecb93
Chris <chris@onerockwell.com>
2016-03-07 12:50:34 -0500

    add new setup

---
+++ src/components/ControlPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from '../styles/control-panel.scss';
+
+
+export default React.createClass({
+	render () {
+		return (
+			<div className='ctrlpanel'>Control Panel</div>
+		)
+	}
+});
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import 'babel-core/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute } from 'react-router';
-import configureStore from './store/configureStore';
-import App from './components/App';
-import HomePage from './components/pages/HomePage';
-import CounterPage from './components/pages/CounterPage';
-import history from './router/history';
 
-const store = configureStore();
+import ControlPanel from './components/ControlPanel';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={HomePage} />
-        <Route path="counter" component={CounterPage} />
-        <Route path="home" component={HomePage} />
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<ControlPanel />, document.getElementById('root'));
+
+
+
