780d981ea1601e4e5b48aabddca092504345fd6c
Chris <cpachomski@gmail.com>
2016-03-08 15:00:01 -0500

    setup componentDidMount for canvas attributes

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,9 +6,32 @@ export default React.createClass({
 		return {
 			backgroundColor: '#000000',
 			lineColor: '#FFFFFF',
+			lineWidth: 1,
 			autoDraw: false,
 			graphFunc: 'clickConnect'
 		}
+	},
+
+	componentDidMount () {
+		this.canvas = document.getElementById('canvas');
+		this.context = canvas.getContext('2d');
+		this.context.imageSmoothingEnabled = true;
+
+		this.setCanvasSize();
+		window.addEventListener('resize', this.setCanvasSize);
+		this.applyStyles();
+	},
+
+	applyStyles () {
+		this.canvas.style.background =  this.state.backgroundColor;
+		this.canvas.lineWidth = this.state.lineWidth;
+		this.canvas.strokeStyle = this.state.lineColor;
+	},
+
+	setCanvasSize () {
+		console.log('resize')
+		this.canvas.width = innerWidth;
+		this.canvas.height = window.innerHeight;
 	},
 
 	render () {
